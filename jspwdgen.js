7f6acc294214a8c7204dce65d32a7efeaa7e72f1
Adriano Marcio Monteiro <adrianomarcmont@users.noreply.github.com>
2022-11-03 20:43:20 -0300

    Create jspwdgen.js

---
+++ jspwdgen.js
@@ -0,0 +1,15 @@
+var p="";
+for(;;) {
+    var rnd=Math.floor(Math.random()*128);
+    if(rnd>=33&&rnd<=126){	
+        p += String.fromCharCode(rnd);
+    }
+    if(p.length==16){
+        if(p.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])((?=.*\W)|(?=.*_))^[^ ]+$/g)){
+			break;
+		}else{
+            p="";
+        }	
+    }
+}
+alert(p);
