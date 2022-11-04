1dded4fac3c2e6a561ba9ca9765c16fd79d04d6c
Adriano Marcio Monteiro <adrianomarcmont@users.noreply.github.com>
2022-11-04 10:16:24 -0300

    Update jspwdgen.js

--- jspwdgen.js
+++ jspwdgen.js
@@ -1,14 +1,14 @@
-var p="";
+var p = "";
 for(;;) {
-    var rnd=Math.floor(Math.random()*128);
-    if(rnd>=33&&rnd<=126){	
+    var rnd = Math.floor(Math.random() * 128);
+    if(rnd >= 33 & rnd <= 126){	
         p += String.fromCharCode(rnd);
     }
-    if(p.length==16){
+    if(p.length == 16){
         if(p.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z])((?=.*\W)|(?=.*_))^[^ ]+$/g)){
 			break;
 		}else{
-            p="";
+            p = "";
         }	
     }
 }
